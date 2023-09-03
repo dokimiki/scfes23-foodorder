16107d383033b1d1e10e6704a12bfe5592ae749c
どきみき <lmznd6yiew@gmail.com>
2023-09-04 00:30:42 +0900

    allergen view created by components

--- src/app/[shop_id]/order/menus/[menu_id].tsx/page.tsx
+++ src/app/[shop_id]/order/menus/[menu_id].tsx/page.tsx
@@ -1,54 +1,56 @@
 "use client";
+import Image from "next/image";
 // メニュー詳細ページ
 import style from "./style.module.css";
+
+function Allergen({
+    allergenName,
+    imgSrc,
+    ContaminationStatus,
+}: {
+    allergenName: string;
+    imgSrc: string;
+    ContaminationStatus: "Contains" | "NotContains" | "Contamination";
+}) {
+    const bgColor = ContaminationStatus === "Contains" ? style.red : ContaminationStatus === "Contamination" ? style.yellow : style.white;
+    return (
+        <p className={bgColor}>
+            <Image src={imgSrc} alt={allergenName} width={50} height={50} />
+            <span>{allergenName}</span>
+        </p>
+    );
+}
 
 export default function MenuDetail() {
     return (
         <main className={style.main}>
-            <span className={style.tornado}><img src="/img/menu_tornado.png" alt="トルネードポテト" /></span>
+            <span className={style.tornado}>
+                <img src="/img/menu_tornado.png" alt="トルネードポテト" />
+            </span>
             <div className={style.position}>
                 <h1>トルネードポテト（塩）</h1>
-                <p>おいしいポテトです!!トルネードになったところを
-                    見たことがあります。あなたはどうですか？塩味の利い
-                    たトルネードポテト、ポテトですね。ぱりぱりうまう
-                    まですよ。あなたもおひとついかがですか？みたこと
+                <p>
+                    おいしいポテトです!!トルネードになったところを 見たことがあります。あなたはどうですか？塩味の利い
+                    たトルネードポテト、ポテトですね。ぱりぱりうまう まですよ。あなたもおひとついかがですか？みたこと
                     がないようなきがするような新しい新感覚ぽてと
                 </p>
                 <div className={style.allergy}>
                     <h2>アレルゲン</h2>
                     <p className={style.white}>白：ふくまれていない</p>
                     <p className={style.yellow}>黄色：調理工程で触れている</p>
-                    <p className={style.red}>赤：含まれている</p>     
+                    <p className={style.red}>赤：含まれている</p>
                 </div>
                 <div className={style.container}>
-                    <p className={style.white}>
-                        <img src="/img/allergen_kani.png" alt="カニ" />
-                        <span>カニ</span>
-                    </p>
-                    <p className={style.white}>
-                        <img src="/img/allergen_milk.png" alt="牛乳" />
-                        <span>牛乳</span>
-                    </p>
-                    <p className={style.white}>
-                        <img src="/img/allergen_kurumi.png" alt="クルミ" />
-                        <span>クルミ</span>
-                    </p>
-                    <p className={style.red}>
-                        <img src="/img/allergen_komugi.png" alt="小麦" />
-                        <span>小麦</span>
-                    </p>
-                    <p className={style.yellow}>
-                        <img src="/img/allergen_peanut.png" alt="ピーナッツ" />
-                        <span>ピーナッツ</span>
-                    </p>
-                    <p className={style.white}> 
-                        <img src="/img/allergen_tamago.png" alt="たまご" />
-                        <span>たまご</span>
-                    </p>
+                    <Allergen allergenName="えび" imgSrc="/img/allergen_ebi.png" ContaminationStatus="Contains" />
+                    <Allergen allergenName="かに" imgSrc="/img/allergen_kani.png" ContaminationStatus="NotContains" />
+                    <Allergen allergenName="小麦" imgSrc="/img/allergen_komugi.png" ContaminationStatus="Contamination" />
+                    <Allergen allergenName="そば" imgSrc="/img/allergen_soba.png" ContaminationStatus="NotContains" />
+                    <Allergen allergenName="卵" imgSrc="/img/allergen_tamago.png" ContaminationStatus="Contains" />
+                    <Allergen allergenName="ミルク" imgSrc="/img/allergen_milk.png" ContaminationStatus="NotContains" />
+                    <Allergen allergenName="落花生" imgSrc="/img/allergen_peanuts.png" ContaminationStatus="NotContains" />
+                    <Allergen allergenName="クルミ" imgSrc="/img/allergen_kurumi.png" ContaminationStatus="NotContains" />
                 </div>
-
             </div>
-
         </main>
     );
 }
