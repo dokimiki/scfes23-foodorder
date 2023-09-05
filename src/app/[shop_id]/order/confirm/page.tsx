e55b8a9d1b7701bc1ef6626ce999ac9f91f77b4f
Daiki Matsushita(松下大暉) <85777234+Matsudai-bit@users.noreply.github.com>
2023-09-06 03:22:59 +0900

    wip

--- src/app/[shop_id]/order/confirm/page.tsx
+++ src/app/[shop_id]/order/confirm/page.tsx
@@ -4,45 +4,56 @@ import { css } from "@emotion/react";
 import style from "./style.module.scss";
 // 注文確認ページ
 
-function Food({}) {
-    const foodName = { 1: "トルネードポテト(塩)", 2: "トルネードポテト(コンソメ)" };
-    const foodNum = { 1: 3, 2: 2 };
-    const foodPrice = { 1: 300, 2: 300 };
+function Food({
+    foodName,
+    foodNum,
+    foodPrice,
+}: {
+    foodName: string;
+    foodNum: number;
+    foodPrice: number;
+}) {
     return (
         <p className={style.food}>
             <div>
-                <span>{foodName[1]}</span>
+                <span>{foodName}</span>
                 <span>×</span>
-                <span>{foodNum[1]}</span>
+                <span>{foodNum}</span>
             </div>
             <div className={style.price}>
                 <span>¥</span>
-                <span>{foodPrice[1]}</span>
+                <span>{foodPrice}</span>
             </div>
         </p>
     );
 }
 
 export default function Confirm() {
+    const foodName = ["トルネードポテト(塩)", "トルネードポテト(コンソメ)"];
+    const foodNum = [3, 2];
+    const foodPrice = [300, 300];
     return (
         <main className={style.main}>
             <h1>注文内容の最終確認</h1>
-            <p className={style.food}>
-                <div>
-                    <span>トルネードポテト(塩)</span>
-                    <span>×</span>
-                    <span>3</span>
-                </div>
-                <div className={style.price}>
-                    <span>¥</span>
-                    <span>900</span>
-                </div>
-            </p>
+            {foodName.map((name, i) => {
+                return (
+                    <Food
+                        foodName={name}
+                        foodNum={foodNum[i]}
+                        foodPrice={foodPrice[i]}
+                    />
+                );
+            })}
+
             <p className={style.total_price}>
                 <span>合計金額</span>
                 <div>
                     <span>¥</span>
-                    <span>900</span>
+                    <span>
+                        {foodPrice.reduce((sum, price) => {
+                            return sum + price;
+                        }, 0)}
+                    </span>
                 </div>
             </p>
         </main>
--- src/app/[shop_id]/order/menus/[menu_id]/page.tsx
+++ src/app/[shop_id]/order/menus/[menu_id]/page.tsx
@@ -14,7 +14,11 @@ import Typography from "@mui/material/Typography";
 import gray from "@mui/material/colors/grey";
 import { Divider } from "@mui/material";
 
-const allergenColor = { Contains: "#ff6347", Contamination: "#ffd700", NotContains: "white" };
+const allergenColor = {
+    Contains: "#ff6347",
+    Contamination: "#ffd700",
+    NotContains: "white",
+};
 
 function Allergen({
     allergenName,
@@ -27,7 +31,18 @@ function Allergen({
 }) {
     const bgColor = allergenColor[ContaminationStatus];
     return (
-        <Stack direction="row" alignItems="center" sx={{ background: bgColor, borderRadius: 2, padding: "2px", minWidth: "130px", margin: 1 }} spacing={1}>
+        <Stack
+            direction="row"
+            alignItems="center"
+            sx={{
+                background: bgColor,
+                borderRadius: 2,
+                padding: "2px",
+                minWidth: "130px",
+                margin: 1,
+            }}
+            spacing={1}
+        >
             <Image src={imgSrc} alt={allergenName} width={50} height={50} />
             <Typography variant="h6">{allergenName}</Typography>
         </Stack>
@@ -64,8 +79,10 @@ export default function MenuDetail() {
                         トルネードポテト（塩）
                     </Typography>
                     <Typography variant="body1">
-                        おいしいポテトです!!トルネードになったところを 見たことがあります。あなたはどうですか？塩味の利い
-                        たトルネードポテト、ポテトですね。ぱりぱりうまう まですよ。あなたもおひとついかがですか？みたこと
+                        おいしいポテトです!!トルネードになったところを
+                        見たことがあります。あなたはどうですか？塩味の利い
+                        たトルネードポテト、ポテトですね。ぱりぱりうまう
+                        まですよ。あなたもおひとついかがですか？みたこと
                         がないようなきがするような新しい新感覚ぽてと
                     </Typography>
 
@@ -82,25 +99,71 @@ export default function MenuDetail() {
                             }
                         `}
                     >
-                        <Typography variant="h6" sx={{ background: allergenColor["NotContains"] }}>
+                        <Typography
+                            variant="h6"
+                            sx={{ background: allergenColor["NotContains"] }}
+                        >
                             白：ふくまれていない
                         </Typography>
-                        <Typography variant="h6" sx={{ background: allergenColor["Contamination"] }}>
+                        <Typography
+                            variant="h6"
+                            sx={{ background: allergenColor["Contamination"] }}
+                        >
                             黄色：調理工程で触れている
                         </Typography>
-                        <Typography variant="h6" sx={{ background: allergenColor["Contains"] }}>
+                        <Typography
+                            variant="h6"
+                            sx={{ background: allergenColor["Contains"] }}
+                        >
                             赤：含まれている
                         </Typography>
                     </Stack>
-                    <Stack direction="row" sx={{ margin: 2 }} flexWrap="wrap" justifyContent="space-around">
-                        <Allergen allergenName="えび" imgSrc="/img/allergen_ebi.png" ContaminationStatus="Contains" />
-                        <Allergen allergenName="かに" imgSrc="/img/allergen_kani.png" ContaminationStatus="NotContains" />
-                        <Allergen allergenName="小麦" imgSrc="/img/allergen_komugi.png" ContaminationStatus="Contamination" />
-                        <Allergen allergenName="そば" imgSrc="/img/allergen_soba.png" ContaminationStatus="NotContains" />
-                        <Allergen allergenName="卵" imgSrc="/img/allergen_tamago.png" ContaminationStatus="Contains" />
-                        <Allergen allergenName="ミルク" imgSrc="/img/allergen_milk.png" ContaminationStatus="NotContains" />
-                        <Allergen allergenName="落花生" imgSrc="/img/allergen_peanuts.png" ContaminationStatus="NotContains" />
-                        <Allergen allergenName="クルミ" imgSrc="/img/allergen_kurumi.png" ContaminationStatus="NotContains" />
+                    <Stack
+                        direction="row"
+                        sx={{ margin: 2 }}
+                        flexWrap="wrap"
+                        justifyContent="space-around"
+                    >
+                        <Allergen
+                            allergenName="えび"
+                            imgSrc="/img/allergen_ebi.png"
+                            ContaminationStatus="Contains"
+                        />
+                        <Allergen
+                            allergenName="かに"
+                            imgSrc="/img/allergen_kani.png"
+                            ContaminationStatus="NotContains"
+                        />
+                        <Allergen
+                            allergenName="小麦"
+                            imgSrc="/img/allergen_komugi.png"
+                            ContaminationStatus="Contamination"
+                        />
+                        <Allergen
+                            allergenName="そば"
+                            imgSrc="/img/allergen_soba.png"
+                            ContaminationStatus="NotContains"
+                        />
+                        <Allergen
+                            allergenName="卵"
+                            imgSrc="/img/allergen_tamago.png"
+                            ContaminationStatus="Contains"
+                        />
+                        <Allergen
+                            allergenName="ミルク"
+                            imgSrc="/img/allergen_milk.png"
+                            ContaminationStatus="NotContains"
+                        />
+                        <Allergen
+                            allergenName="落花生"
+                            imgSrc="/img/allergen_peanuts.png"
+                            ContaminationStatus="NotContains"
+                        />
+                        <Allergen
+                            allergenName="クルミ"
+                            imgSrc="/img/allergen_kurumi.png"
+                            ContaminationStatus="NotContains"
+                        />
                     </Stack>
                 </div>
             </div>
@@ -112,10 +175,18 @@ export default function MenuDetail() {
                     background-color: white;
                 `}
             >
-                <Stack direction="row" justifyContent="space-between" alignItems="center" sx={{ margin: 2 }}>
+                <Stack
+                    direction="row"
+                    justifyContent="space-between"
+                    alignItems="center"
+                    sx={{ margin: 2 }}
+                >
                     <Stack direction="row" alignItems="baseline">
                         <Typography variant="h5">金額: </Typography>
-                        <Typography variant="h6" sx={{ color: "text.secondary" }}>
+                        <Typography
+                            variant="h6"
+                            sx={{ color: "text.secondary" }}
+                        >
                             ￥
                         </Typography>
                         <Typography variant="h4">300</Typography>
@@ -140,7 +211,11 @@ export default function MenuDetail() {
                     </Stack>
                 </Stack>
 
-                <Stack direction="row" justifyContent="space-around" sx={{ margin: 2 }}>
+                <Stack
+                    direction="row"
+                    justifyContent="space-around"
+                    sx={{ margin: 2 }}
+                >
                     <Button variant="outlined" sx={{ color: "black" }}>
                         注文を確定する
                     </Button>
