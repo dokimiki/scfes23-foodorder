a847beff5611405094af58f69563ad49c3a236be
Daiki Matsushita(松下大暉) <85777234+Matsudai-bit@users.noreply.github.com>
2023-09-06 02:34:10 +0900

    wip

--- src/app/[shop_id]/order/confirm/page.tsx
+++ src/app/[shop_id]/order/confirm/page.tsx
@@ -4,22 +4,48 @@ import { css } from "@emotion/react";
 import style from "./style.module.scss";
 // 注文確認ページ
 
+function Food({
+}) {
+    const foodName = {1:"トルネードポテト(塩)",2:"トルネードポテト(コンソメ)"};
+    const foodNum = {1:3,2:2}};
+    const foodPrice = {1:300,2:300};
+    return (
+        <p className={style.food}>
+            <div>
+                <span>{foodName}</span>
+                <span>×</span>
+                <span>{foodNum}</span>
+            </div>
+            <div className={style.price}>
+                <span>¥</span>
+                <span>{foodPrice}</span>
+            </div>
+        </p>
+    );
+
 
 
 export default function Confirm() {
     return (
-        <main>
+        <main className={style.main}>
             <h1>注文内容の最終確認</h1>
-            <p>
+            <p className={style.food}>
                 <div>
-
-                    
+                    <span>トルネードポテト(塩)</span>
+                    <span>×</span>
+                    <span>3</span>
                 </div>
-                <span>トルネードポテト(塩)</span>
-                <span>×</span>
-                <span>3</span>
-                <span>¥</span>
-                <span>900</span>
+                <div className={style.price}>
+                    <span>¥</span>
+                    <span>900</span>
+                </div>
+            </p>
+            <p className={style.total_price}>
+                <span>合計金額</span>
+                <div>
+                    <span>¥</span>
+                    <span>900</span>
+                </div>
             </p>
 
         </main>
