68c2407df16de676a3dda52f70b186e4dda1f62a
どきみき <lmznd6yiew@gmail.com>
2023-09-06 02:36:20 +0900

    fix error

--- src/app/[shop_id]/order/confirm/page.tsx
+++ src/app/[shop_id]/order/confirm/page.tsx
@@ -4,26 +4,24 @@ import { css } from "@emotion/react";
 import style from "./style.module.scss";
 // 注文確認ページ
 
-function Food({
-}) {
-    const foodName = {1:"トルネードポテト(塩)",2:"トルネードポテト(コンソメ)"};
-    const foodNum = {1:3,2:2}};
-    const foodPrice = {1:300,2:300};
+function Food({}) {
+    const foodName = { 1: "トルネードポテト(塩)", 2: "トルネードポテト(コンソメ)" };
+    const foodNum = { 1: 3, 2: 2 };
+    const foodPrice = { 1: 300, 2: 300 };
     return (
         <p className={style.food}>
             <div>
-                <span>{foodName}</span>
+                <span>{foodName[1]}</span>
                 <span>×</span>
-                <span>{foodNum}</span>
+                <span>{foodNum[1]}</span>
             </div>
             <div className={style.price}>
                 <span>¥</span>
-                <span>{foodPrice}</span>
+                <span>{foodPrice[1]}</span>
             </div>
         </p>
     );
-
-
+}
 
 export default function Confirm() {
     return (
@@ -47,7 +45,6 @@ export default function Confirm() {
                     <span>900</span>
                 </div>
             </p>
-
         </main>
     );
 }
