cad17082567ffe0871314d8fcb8017515da72071
どきみき <lmznd6yiew@gmail.com>
2023-09-13 23:42:22 +0900

    fix error

--- src/app/[shop_id]/admin/layout.tsx
+++ src/app/[shop_id]/admin/layout.tsx
@@ -1,23 +1,14 @@
 "use client";
 import * as React from "react";
-import Head from "next/head";
-import ThemeRegistry from "@/components/ThemeRegistry/ThemeRegistry";
 import Header from "@/components/layouts/AdminHeader";
 import Footer from "@/components/layouts/AdminFooter";
 
 export default function Layout({ children, params }: { children: React.ReactNode; params: any }) {
     return (
-        <html lang="jp">
-            <Head>
-                <title>文化祭フードオーダー</title>
-            </Head>
-            <body>
-                <ThemeRegistry>
-                    <Header />
-                    {children}
-                    <Footer />
-                </ThemeRegistry>
-            </body>
-        </html>
+        <>
+            <Header />
+            {children}
+            <Footer />
+        </>
     );
 }
--- src/app/[shop_id]/order/layout.tsx
+++ src/app/[shop_id]/order/layout.tsx
@@ -1,23 +1,14 @@
 "use client";
 import * as React from "react";
-import Head from "next/head";
-import ThemeRegistry from "@/components/ThemeRegistry/ThemeRegistry";
 import Header from "@/components/layouts/OrderHeader";
 import Footer from "@/components/layouts/OrderFooter";
 
 export default function Layout({ children, params }: { children: React.ReactNode; params: any }) {
     return (
-        <html lang="jp">
-            <Head>
-                <title>文化祭フードオーダー</title>
-            </Head>
-            <body>
-                <ThemeRegistry>
-                    <Header title={"小林トルネード(I3)"} homeLink={"/" + params.shop_id + "/order/menus"} />
-                    {children}
-                    <Footer />
-                </ThemeRegistry>
-            </body>
-        </html>
+        <>
+            <Header title={"小林トルネード(I3)"} homeLink={"/" + params.shop_id + "/order/menus"} />
+            {children}
+            <Footer />
+        </>
     );
 }
