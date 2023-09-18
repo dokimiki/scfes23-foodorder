9902da2cce8d8a02b830a72034b9bf9c04bb5c06
どきみき <lmznd6yiew@gmail.com>
2023-09-19 03:57:47 +0900

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,11 +3,6 @@ import * as React from "react";
 import Head from "next/head";
 import ThemeRegistry from "@/components/ThemeRegistry/ThemeRegistry";
 
-export const metadata = {
-    title: "Next.js App Router + Material UI v5",
-    description: "Next.js App Router + Material UI v5",
-};
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="jp">
