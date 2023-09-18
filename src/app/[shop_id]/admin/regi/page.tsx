569f52a3704cb1270949d6eca725e7f05abd450f
どきみき <lmznd6yiew@gmail.com>
2023-09-18 22:52:38 +0900

    店員側ちょうせ　

--- src/app/[shop_id]/admin/regi/page.tsx
+++ src/app/[shop_id]/admin/regi/page.tsx
@@ -1,113 +1,75 @@
 "use client";
 import * as React from "react";
-import { css } from "@emotion/react";
+import { Global, css } from "@emotion/react";
 import { Badge, Button, Card, CardMedia, Paper, Stack, Typography, IconButton } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
 import Stepper from "@/components/Stepper";
 import { enqueueSnackbar } from "notistack";
+import { getMenuItems } from "@/libs/Items";
+import { CartItem, MenuItem } from "@/libs/types/item";
+import { MAX_CART_ITEM_QUANTITY, MIN_CART_ITEM_QUANTITY } from "@/libs/Carts";
 // レジのページ
 
 export default function Regi() {
-    type item = {
-        id: number;
-        number: number;
-    };
+    const [menus, setMenus] = React.useState<MenuItem[]>([]);
 
-    const [itemSelects, setItemSelects] = React.useState([
-        { id: 1, number: 1 },
-        { id: 2, number: 1 },
-        { id: 3, number: 1 },
-        { id: 4, number: 0 },
-        { id: 5, number: 0 },
-        { id: 6, number: 0 },
-        { id: 7, number: 0 },
-        { id: 8, number: 0 },
-        { id: 9, number: 0 },
-        { id: 10, number: 0 },
-    ] as item[]);
+    React.useEffect(() => {
+        getMenuItems()
+            .then((res) => {
+                setMenus(res);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }, []);
 
-    function addItem(itemIndex: number) {
-        let newItemSelects = itemSelects.slice();
+    const [cart, setCart] = React.useState([] as CartItem[]);
 
-        newItemSelects[itemIndex].number++;
-
-        if (newItemSelects[itemIndex].number > 5) {
-            enqueueSnackbar("これ以上増やせません", { variant: "error" });
-            newItemSelects[itemIndex].number = 5;
+    function addToCart(id: string) {
+        const index = cart.findIndex((e) => e.id === id);
+        if (index === -1) {
+            setCart([...cart, { id: id, quantity: 1 }]);
+        } else {
+            const newCart = cart.slice();
+            newCart[index].quantity++;
+            newCart[index].quantity = Math.min(newCart[index].quantity, MAX_CART_ITEM_QUANTITY);
+            setCart(newCart);
         }
-
-        setItemSelects(newItemSelects);
     }
 
-    function removeItem(itemIndex: number) {
-        let newItemSelects = itemSelects.slice();
+    function removeFromCart(id: string) {
+        const index = cart.findIndex((e) => e.id === id);
+        if (index === -1) {
+            return;
+        } else {
+            const newCart = cart.slice();
+            newCart[index].quantity--;
+            newCart[index].quantity = Math.max(newCart[index].quantity, MIN_CART_ITEM_QUANTITY);
+            if (newCart[index].quantity <= MIN_CART_ITEM_QUANTITY) {
+                enqueueSnackbar((menus.find((e) => e.id === newCart[index].id)?.name ?? "なぞ") + "をカートから削除しました。", {
+                    variant: "info",
+                });
 
-        newItemSelects[itemIndex].number--;
-
-        if (newItemSelects[itemIndex].number < 0) {
-            enqueueSnackbar("これ以上減らせません。", { variant: "error" });
-            newItemSelects[itemIndex].number = 0;
+                newCart.splice(index, 1);
+            }
+            setCart(newCart);
         }
-
-        setItemSelects(newItemSelects);
     }
 
-    function deleteItem(itemIndex: number) {
-        let newItemSelects = itemSelects.slice();
+    function deleteFromCart(id: string) {
+        const index = cart.findIndex((e) => e.id === id);
+        if (index === -1) {
+            return;
+        } else {
+            const newCart = cart.slice();
 
-        newItemSelects[itemIndex].number = 0;
-        setItemSelects(newItemSelects);
+            enqueueSnackbar((menus.find((e) => e.id === newCart[index].id)?.name ?? "なぞ") + "をカートから削除しました。", {
+                variant: "info",
+            });
 
-        enqueueSnackbar("会計からアイテムを削除しました。", { variant: "info" });
-    }
-
-    function ItemPaper({
-        img,
-        title,
-        price,
-        num,
-        itemIndex,
-    }: {
-        img: string;
-        title: string;
-        price: number;
-        num: number;
-        itemIndex: number;
-    }) {
-        return (
-            <Button onClick={() => addItem(itemIndex)} sx={{ margin: 2, padding: 0 }}>
-                <Badge
-                    badgeContent={num}
-                    color="primary"
-                    css={css`
-                        & > .MuiBadge-badge {
-                            width: 30px;
-                            height: 30px;
-                            border-radius: 15px;
-                            font-size: 17px;
-                            font-weight: 400;
-                        }
-                    `}
-                >
-                    <Card sx={{ width: 200 }}>
-                        <CardMedia sx={{ height: 160 }} image={img} />
-                        <Stack sx={{ padding: "16px" }}>
-                            <Typography gutterBottom variant="body2" fontWeight="500" align="left">
-                                {title}
-                            </Typography>
-                            <Stack direction="row" alignItems="baseline">
-                                <Typography variant="body2" fontWeight="400">
-                                    売価:&nbsp;
-                                </Typography>
-                                <Typography variant="h6" fontWeight="500">
-                                    ¥{price}
-                                </Typography>
-                            </Stack>
-                        </Stack>
-                    </Card>
-                </Badge>
-            </Button>
-        );
+            newCart.splice(index, 1);
+            setCart(newCart);
+        }
     }
 
     const sideBarWidth = 350;
@@ -122,14 +84,12 @@ export default function Regi() {
                     alignContent="flex-start"
                     justifyContent="flex-start"
                 >
-                    {itemSelects.map((e, i) => {
+                    {menus.map((e, i) => {
                         return (
-                            <ItemPaper
-                                img={"https://images.unsplash.com/photo-1552845683-cfefc701e423?w=1887&q=80"}
-                                title={"トルネードポテト(塩)" + e.id}
-                                price={120}
-                                num={e.number}
-                                itemIndex={i}
+                            <MenuItemPaper
+                                itemMenuInfo={e}
+                                itemCartInfo={cart.find((cart) => cart.id === e.id)}
+                                addToCart={() => addToCart(e.id)}
                                 key={i}
                             />
                         );
@@ -148,48 +108,121 @@ export default function Regi() {
                         width: `${sideBarWidth}px`,
                     }}
                 >
-                    <div>
-                        {itemSelects.map((e, i) => {
-                            const title = "トルネードポテト(塩)" + e.id;
-                            const price = 120;
-                            return (
-                                <Paper
-                                    key={i}
-                                    sx={{
-                                        transition:
-                                            "max-height 2s cubic-bezier(0.2, 0.3, 0.8, 0.7)" +
-                                            ", margin-bottom 0.2s ease-out" +
-                                            (e.number > 0 ? "" : " 2.2s"),
-                                        overflow: "hidden",
-                                        height: "auto",
-                                        maxHeight: e.number > 0 ? "100vh" : "0px",
-                                        marginBottom: e.number > 0 ? 2 : 0,
-                                    }}
-                                >
-                                    <Typography variant="h6">{title}</Typography>
-                                    <Stack direction="row" justifyContent="space-between" alignItems="center" sx={{ margin: 1 }}>
-                                        <Stack direction="row" alignItems="flex-end">
-                                            <Typography variant="subtitle1">合計:&nbsp;</Typography>
-                                            <Typography variant="h5">¥{price * e.number}</Typography>
-                                        </Stack>
-                                        <Stack direction="row">
-                                            <IconButton aria-label="delete" onClick={() => deleteItem(i)}>
-                                                <DeleteIcon fontSize="small" color="error" />
-                                            </IconButton>
-                                            <Stepper
-                                                num={e.number}
-                                                onClickAdd={() => addItem(i)}
-                                                onClickRemove={() => removeItem(i)}
-                                                size="small"
-                                            />
-                                        </Stack>
-                                    </Stack>
-                                </Paper>
-                            );
-                        })}
-                    </div>
+                    {cart.map((e, i) => {
+                        return (
+                            <CartItemPaper
+                                itemMenuInfo={menus.find((menu) => menu.id === e.id)}
+                                itemCartInfo={e}
+                                addToCart={() => addToCart(e.id)}
+                                removeFromCart={() => removeFromCart(e.id)}
+                                deleteFromCart={() => deleteFromCart(e.id)}
+                                key={i}
+                            />
+                        );
+                    })}
                 </Stack>
             </Stack>
         </main>
+    );
+}
+
+function MenuItemPaper({
+    itemMenuInfo,
+    itemCartInfo,
+    addToCart,
+}: {
+    itemMenuInfo: MenuItem;
+    itemCartInfo?: CartItem;
+    addToCart: () => void;
+}) {
+    return (
+        <Button onClick={addToCart} sx={{ margin: 2, padding: 0 }}>
+            <Badge
+                badgeContent={itemCartInfo?.quantity ?? 0}
+                color="primary"
+                css={css`
+                    & > .MuiBadge-badge {
+                        width: 30px;
+                        height: 30px;
+                        border-radius: 15px;
+                        font-size: 17px;
+                        font-weight: 400;
+                    }
+                `}
+            >
+                <Card sx={{ width: 200 }}>
+                    <CardMedia sx={{ height: 160 }} image={itemMenuInfo.image} />
+                    <Stack sx={{ padding: "16px" }}>
+                        <Typography gutterBottom variant="body2" fontWeight="500" align="left">
+                            {itemMenuInfo.name}
+                        </Typography>
+                        <Stack direction="row" alignItems="baseline">
+                            <Typography variant="body2" fontWeight="400">
+                                売価:&nbsp;
+                            </Typography>
+                            <Typography variant="h6" fontWeight="500">
+                                ¥{itemMenuInfo.price}
+                            </Typography>
+                        </Stack>
+                    </Stack>
+                </Card>
+            </Badge>
+        </Button>
+    );
+}
+
+function CartItemPaper({
+    itemMenuInfo,
+    itemCartInfo,
+    addToCart,
+    removeFromCart,
+    deleteFromCart,
+}: {
+    itemMenuInfo?: MenuItem;
+    itemCartInfo: CartItem;
+    addToCart: () => void;
+    removeFromCart: () => void;
+    deleteFromCart: () => void;
+}) {
+    return (
+        <div>
+            <Global
+                styles={{
+                    ".cart-item-paper": {},
+                }}
+            />
+            <Paper
+                className={"cart-item-paper" + (itemCartInfo.quantity <= MIN_CART_ITEM_QUANTITY ? " cart-item-paper-hidden" : "")}
+                sx={{
+                    transition:
+                        "max-height 2s cubic-bezier(0.2, 0.3, 0.8, 0.7)" +
+                        ", margin-bottom 0.2s ease-out" +
+                        (itemCartInfo.quantity > 0 ? "" : " 2.2s"),
+                    overflow: "hidden",
+                    height: "auto",
+                    maxHeight: itemCartInfo.quantity > 0 ? "100vh" : "0px",
+                    marginBottom: itemCartInfo.quantity > 0 ? 2 : 0,
+                }}
+            >
+                <Typography variant="h6">{itemMenuInfo?.name ?? "なぞ"}</Typography>
+                <Stack direction="row" justifyContent="space-between" alignItems="center" sx={{ margin: 1 }}>
+                    <Stack direction="row" alignItems="flex-end">
+                        <Typography variant="subtitle1">合計:&nbsp;</Typography>
+                        <Typography variant="h5">¥{(itemMenuInfo?.price ?? 0) * itemCartInfo.quantity}</Typography>
+                    </Stack>
+                    <Stack direction="row">
+                        <IconButton aria-label="delete" onClick={() => deleteFromCart()}>
+                            <DeleteIcon fontSize="small" color="error" />
+                        </IconButton>
+                        <Stepper
+                            num={itemCartInfo.quantity}
+                            onClickAdd={() => addToCart()}
+                            onClickRemove={() => removeFromCart()}
+                            size="small"
+                        />
+                    </Stack>
+                </Stack>
+            </Paper>
+        </div>
     );
 }
