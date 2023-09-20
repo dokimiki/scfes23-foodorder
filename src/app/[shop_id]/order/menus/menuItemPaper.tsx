74ef47bf837429e873638a4eaa9b56b3cf755716
工業高校(21年度) 生徒0196 <eduh012-hs210196@giga.nagoya-c.ed.jp>
2023-09-20 16:21:32 +0900

    wip

--- src/app/[shop_id]/order/menus/menuItemPaper.tsx
+++ src/app/[shop_id]/order/menus/menuItemPaper.tsx
@@ -34,7 +34,7 @@ export default function MenuItemPaper({ name , price}: { name: string ,price: nu
                     </Avatar>
                 </ListItemAvatar>
                     
-                    <ListItemText primary={name} secondary={price} /><p>円</p>
+                    <ListItemText  secondary={price} primary={name} /><p>円</p>
 
                         <Button size="medium" sx={{fontSize: "0.7rem",}}>
                             アレルギー情報<InfoIcon fontSize="medium" htmlColor="#FECC4E" />
