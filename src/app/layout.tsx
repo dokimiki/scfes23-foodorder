1351d9bad35982f7fbde6bc4d30441f6db083530
dokimiki <lmznd6yiew@gmail.com>
2023-09-21 16:03:41 +0900

    Revert "wip"
    This reverts commit 3b53b12e2b50b2ab948ac28227d12f727150a041.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,13 @@ import { css } from "@emotion/react";
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="jp">
-            <body>
+            <body
+                css={css`
+                    height: 100vh;
+                    overflow: scroll;
+                    -webkit-overflow-scrolling: touch;
+                `}
+            >
                 <Head>
                     <title>nextApp</title>
                     <meta name="apple-mobile-web-app-capable" content="yes" />
