77992f036d102e1933cab468cc55896ae3f5ccf0
工業高校(21年度) 生徒0196 <eduh012-hs210196@giga.nagoya-c.ed.jp>
2023-09-21 15:14:33 +0900

    wip

--- src/app/[shop_id]/admin/potato/page.tsx
+++ src/app/[shop_id]/admin/potato/page.tsx
@@ -21,22 +21,18 @@ function createData(
 const rows = [
   createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
   createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
+  createData('合計', 262, 16.0, 24, 6.0),
 ];
 
-export default function BasicTable() {
+export default function PotatoTable() {
   return (
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
             <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            <TableCell align="right">本数</TableCell>
+            <TableCell align="right">作成時間目標</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
