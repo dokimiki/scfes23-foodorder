060a3f3c4bf6b7409fc5b69a2c03d3fcfe0b3274
dokimiki <lmznd6yiew@gmail.com>
2023-09-21 14:47:53 +0900

    wip

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,18 @@
 import * as React from "react";
 import Head from "next/head";
 import ThemeRegistry from "@/components/ThemeRegistry/ThemeRegistry";
+import { css } from "@emotion/react";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="jp">
-            <body>
+            <body
+                css={css`
+                    height: 100vh;
+                    overflow: scroll;
+                    -webkit-overflow-scrolling: touch;
+                `}
+            >
                 <Head>
                     <title>nextApp</title>
                     <meta name="apple-mobile-web-app-capable" content="yes" />
