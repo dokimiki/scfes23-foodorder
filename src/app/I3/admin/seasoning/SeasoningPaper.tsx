3f2cf15587c6b04ee05b66f442d7f7b21ca9e60a
Daiki Matsushita(松下大暉) <85777234+Matsudai-bit@users.noreply.github.com>
2023-09-25 04:05:15 +0900

    wip

---
+++ src/app/I3/admin/seasoning/SeasoningPaper.tsx
@@ -0,0 +1,55 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
+import Divider from "@mui/material/Divider";
+import Typography from "@mui/material/Typography";
+import { seasoning } from "@/libs/types/seasoning";
+import { orderContent } from "@/libs/types/orderContent";
+import { Stack } from "@mui/material";
+
+export default function SeasoningPaper({ seases, order }: { seases: seasoning[]; order: orderContent }) {
+    return (
+        <Box sx={{ width: "100%", maxWidth: 900, bgcolor: "background.paper", margin: "0 auto" }}>
+            <Stack direction="row" justifyContent={"space-between"} alignItems="center">
+                <Stack display={"grid"} gridTemplateColumns={"1fr 1fr 1fr"}>
+                    {seases.map((e, i) => (
+                        <Content name={e.seasoningName} qty={e.qty} key={i} />
+                    ))}
+                </Stack>
+
+                <Box width={"120px"} textAlign={"center"}>
+                    {order.isMobilOrder ? (
+                        <Typography fontSize={"1.2rem"} borderBottom={"1px solid black"} marginTop={"10px"}>
+                            モバイル注文
+                        </Typography>
+                    ) : (
+                        <Typography fontSize={"1.2rem"} borderBottom={"1px solid black"} marginTop={"10px"}>
+                            店内注文
+                        </Typography>
+                    )}
+                    <Typography fontSize={"1.0rem"}>お客様番号 : {order.orderNumber}</Typography>
+                    <Button variant="contained" size="small" sx={{ margin: "20px 0" }}>
+                        <Typography fontSize={"1.2rem"}>完了</Typography>
+                    </Button>
+                </Box>
+            </Stack>
+            <Divider variant="middle" />
+        </Box>
+    );
+}
+
+function Content({ name, qty }: { name: string; qty: number }) {
+    return (
+        <Box>
+            <Stack direction="row" justifyContent={"center"}>
+                <Typography gutterBottom variant="h6" component="div">
+                    {name} :
+                </Typography>
+                <Typography gutterBottom variant="h6" component="div" marginLeft={"5px"}>
+                    {qty}
+                </Typography>
+            </Stack>
+        </Box>
+    );
+}
---
+++ src/libs/types/seasoning.ts
@@ -0,0 +1,1 @@
+export type seasoning = { seasoningName: string; qty: number;};
