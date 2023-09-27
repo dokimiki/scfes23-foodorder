8a19e85ffceefa06f7366d3738b1dbbac029c88c
daiki <none>
2023-09-27 13:19:18 +0900

    wip

--- src/app/I3/order/confirm/page.tsx
+++ src/app/I3/order/confirm/page.tsx
@@ -90,7 +90,7 @@ export default function Confirm() {
 
                 <Card sx={{ margin: "10px auto", backgroundColor: "white" }}>
                     <CardContent>
-                        <Typography sx={{ margin: "0 auto", width: "200px" }}>
+                        <Typography sx={{ margin: "0 auto", width: "340px" }}>
                             <img src="/img/sample_code.jpeg" alt="qrコード" />
                         </Typography>
                         <Button
