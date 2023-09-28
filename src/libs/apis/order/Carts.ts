a592fbeaad0f4b918277ed91d50c2b034ad63bfb
dokimiki <57443812+dokimiki@users.noreply.github.com>
2023-09-29 07:47:01 +0900

    Update Carts.ts

--- src/libs/apis/order/Carts.ts
+++ src/libs/apis/order/Carts.ts
@@ -3,6 +3,6 @@ import { CartItem } from "@/libs/types/item";
 
 export function sendCartData(cart: CartItem[]): Promise<boolean> {
     const token: string = localStorage.getItem("user-id") || "";
-    return fetch("https://ncth-app.jp:3939/v1/user/me/sendcartdata", {
+    return fetch("https://ncth-app.jp:3939/v1/user/me/sendcartdata/" JSON.stringify(cart) + , {
   headers: { 'Authorization': 'Bearer: ' + token} } ).then((res) => !!res);
 }
