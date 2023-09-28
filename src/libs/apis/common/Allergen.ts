22296b391c94d410b77b338ede71fdea93a290f6
dokimiki <57443812+dokimiki@users.noreply.github.com>
2023-09-29 07:04:59 +0900

    Update Allergen.ts

--- src/libs/apis/common/Allergen.ts
+++ src/libs/apis/common/Allergen.ts
@@ -2,20 +2,5 @@
 import { AllergensList } from "@/libs/types/allergen";
 
 export async function getAllergen(menuId: string): Promise<AllergensList> {
-    const mockResponse: AllergensList = {
-        ebi: menuId === "1" ? "Contains" : "NotContains",
-        kani: "NotContains",
-        komugi: "NotContains",
-        kurumi: "NotContains",
-        milk: "NotContains",
-        peanut: "NotContains",
-        soba: "NotContains",
-        tamago: "NotContains",
-    };
-
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve(mockResponse);
-        }, 1000);
-    });
+    return fetch("https://ncth-app.jp:3939/v1/common/allergens/" + menuId).then((res) => res.json());
 }
