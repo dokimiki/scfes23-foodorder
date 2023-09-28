7c870ab5ce578c965f2b068369178894533e5eca
dokimiki <57443812+dokimiki@users.noreply.github.com>
2023-09-29 07:21:20 +0900

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
 "use client";
 import * as React from "react";
-import Link from "next/link";
 
 export default function HomePage() {
     return (
         <>
-            <Link href="/I3/order/menus">お客さん側</Link>
-            <br />
-            <Link href="/I3/admin/regi">店員さん側</Link>
             <p>helllo</p>
         </>
     );
