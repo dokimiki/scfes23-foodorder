cac775e8c90953b6f1ee3ed576e893b2520b02a5
dokimiki <57443812+dokimiki@users.noreply.github.com>
2023-09-29 07:38:50 +0900

    Update Completed.ts

--- src/libs/apis/order/Completed.ts
+++ src/libs/apis/order/Completed.ts
@@ -9,24 +9,7 @@ export function getCompleteState(): Promise<CompleteState> {
 }
 
 export function getCompleteInfo(): Promise<CompleteInfo> {
-    const mockResponse: CompleteInfo = {
-        barcode: "12345678912072313912",
-        completeTime: "18:00",
-        items: [
-            {
-                id: "11",
-                quantity: 2,
-            },
-            {
-                id: "17",
-                quantity: 1,
-            },
-        ],
-    };
-
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve(mockResponse);
-        }, 1000);
-    });
+    const token: string = localStorage.getItem("user-id") || "";
+    return fetch("https://ncth-app.jp:3939/v1/user/me/getcompleteinfo", {
+  headers: { 'Authorization': 'Bearer: ' + token} } ).then((res) => res.json());
 }
