830fbe78c805c0908eaad4f0c3000ec34baa30c5
どきみき <lmznd6yiew@gmail.com>
2023-09-28 20:29:58 +0900

    wip

--- next.config.js
+++ next.config.js
@@ -7,6 +7,9 @@ const nextConfig = {
             transform: '@mui/icons-material/{{member}}',
         },
     },
+    images: {
+        unoptimized: true
+    },
 };
 
 module.exports = nextConfig
