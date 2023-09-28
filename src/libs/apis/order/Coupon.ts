26f74c8dab016a12986bb3440716f30fb48feda3
daiki <none>
2023-09-28 13:40:57 +0900

    wip

--- src/libs/apis/order/Coupon.ts
+++ src/libs/apis/order/Coupon.ts
@@ -30,9 +30,9 @@ export function getCouponItemIds(): Promise<CouponItemIds> {
     const mockResponse: CouponItemIds = {
         none: null,
         "0": null,
-        "100": "7",
-        "200": "8",
-        "300": "9",
+        "100": "16",
+        "200": "17",
+        "300": "18",
     };
 
     return new Promise((resolve, reject) => {
