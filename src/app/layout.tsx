0c282dc3b08f99467a0554ba579dcb67fe70dbe3
dokimiki <lmznd6yiew@gmail.com>
2023-09-28 16:15:38 +0900

    test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,4 @@
-"use client";
-
 import * as React from "react";
-import { Metadata } from "next";
-
-export async function generateMetadata({ params }: { params: any }): Promise<Metadata> {
-    return {
-        title: "I3の予約ページ",
-        description: "",
-        appleWebApp: true,
-    };
-}
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
