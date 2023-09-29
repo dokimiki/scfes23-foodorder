0d58df5145c4b38efcde448eca6ec53a6d07356e
dokimiki <lmznd6yiew@gmail.com>
2023-09-29 11:56:18 +0900

    wip

--- src/libs/apis/order/User.ts
+++ src/libs/apis/order/User.ts
@@ -4,7 +4,7 @@ import { User } from "@/libs/types/user";
 
 export function SignIn(userId: string): Promise<User> {
     const token: string = localStorage.getItem("user-id") || "";
-    return fetch("https://ncth-app.jp:3939/v1/user/me/signin" + token ).then((res) => res.json());
+    return fetch("https://ncth-app.jp:3939/v1/user/me/signin/" + token ).then((res) => res.json());
 }
 
 export function SignUp(): Promise<User> {
