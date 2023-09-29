3a405ded8068551b0fab93114b50ce3817b45e8c
Daiki Matsushita(松下大暉) <85777234+Matsudai-bit@users.noreply.github.com>
2023-09-29 23:40:37 +0900

    wip

--- src/libs/apis/order/Coupon.ts
+++ src/libs/apis/order/Coupon.ts
@@ -1,12 +1,17 @@
 "use client";
 
 import { Coupon, CouponItemIds } from "@/libs/types/coupon";
+import { resolve } from "path";
 
 export function drawBulkLots(): Promise<Coupon> {
     const token: string = localStorage.getItem("user-id") || "";
-    return fetch("https://ncth-app.jp:3939/v1/user/me/drawbulklots", {
-        headers: { Authorization: "Bearer " + token },
-    }).then((res) => res.json());
+    return new Promise(() => {
+        setTimeout(() => {
+            fetch("https://ncth-app.jp:3939/v1/user/me/drawbulklots", {
+                headers: { Authorization: "Bearer " + token },
+            }).then((res) => res.json());
+        }, 1000);
+    });
 }
 
 export function drawInviteLots(): Promise<Coupon> {
