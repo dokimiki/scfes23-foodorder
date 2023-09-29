e13267c8104d5e52114d50d2f1a18cc686cd88bb
dokimiki <lmznd6yiew@gmail.com>
2023-09-29 13:04:42 +0900

    wip

--- src/libs/apis/order/Completed.ts
+++ src/libs/apis/order/Completed.ts
@@ -5,11 +5,13 @@ import { CompleteInfo, CompleteState } from "@/libs/types/orderComplete";
 export function getCompleteState(): Promise<CompleteState> {
     const token: string = localStorage.getItem("user-id") || "";
     return fetch("https://ncth-app.jp:3939/v1/user/me/getcompletestate", {
-  headers: { 'Authorization': 'Bearer: ' + token} } ).then((res) => res.json());
+        headers: { Authorization: "Bearer " + token },
+    }).then((res) => res.json());
 }
 
 export function getCompleteInfo(): Promise<CompleteInfo> {
     const token: string = localStorage.getItem("user-id") || "";
     return fetch("https://ncth-app.jp:3939/v1/user/me/getcompleteinfo", {
-  headers: { 'Authorization': 'Bearer: ' + token} } ).then((res) => res.json());
+        headers: { Authorization: "Bearer " + token },
+    }).then((res) => res.json());
 }
--- src/libs/apis/order/Coupon.ts
+++ src/libs/apis/order/Coupon.ts
@@ -5,17 +5,20 @@ import { Coupon, CouponItemIds } from "@/libs/types/coupon";
 export function drawBulkLots(): Promise<Coupon> {
     const token: string = localStorage.getItem("user-id") || "";
     return fetch("https://ncth-app.jp:3939/v1/user/me/drawbulklots", {
-  headers: { 'Authorization': 'Bearer: ' + token} } ).then((res) => res.json());
+        headers: { Authorization: "Bearer " + token },
+    }).then((res) => res.json());
 }
 
 export function drawInviteLots(): Promise<Coupon> {
     const token: string = localStorage.getItem("user-id") || "";
     return fetch("https://ncth-app.jp:3939/v1/user/me/drawinvitelots", {
-  headers: { 'Authorization': 'Bearer: ' + token} } ).then((res) => res.json());
+        headers: { Authorization: "Bearer " + token },
+    }).then((res) => res.json());
 }
 
 export function getCouponItemIds(): Promise<CouponItemIds> {
     const token: string = localStorage.getItem("user-id") || "";
     return fetch("https://ncth-app.jp:3939/v1/user/me/getcouponitemids", {
-  headers: { 'Authorization': 'Bearer: ' + token} } ).then((res) => res.json());
+        headers: { Authorization: "Bearer " + token },
+    }).then((res) => res.json());
 }
