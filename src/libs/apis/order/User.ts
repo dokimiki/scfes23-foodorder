716e614927e7cb8d83d928ca49a1edbee87a1e58
dokimiki <lmznd6yiew@gmail.com>
2023-09-29 12:14:27 +0900

    commit

--- src/libs/apis/order/User.ts
+++ src/libs/apis/order/User.ts
@@ -1,12 +1,14 @@
 "use client";
 
 import { User } from "@/libs/types/user";
+import jwtDecode from "jwt-decode";
 
 export function SignIn(userId: string): Promise<User> {
-    const token: string = localStorage.getItem("user-id") || "";
-    return fetch("https://ncth-app.jp:3939/v1/user/me/signin/" + token ).then((res) => res.json());
+    const jwt: string = localStorage.getItem("user-id") || "";
+    const token: string = (jwtDecode(jwt) as any).sub;
+    return fetch("https://ncth-app.jp:3939/v1/user/me/signin/" + token).then((res) => res.json());
 }
 
 export function SignUp(): Promise<User> {
-    return fetch("https://ncth-app.jp:3939/v1/user/signup", {method: "POST"}).then((res) => res.json());
+    return fetch("https://ncth-app.jp:3939/v1/user/signup", { method: "POST" }).then((res) => res.json());
 }
